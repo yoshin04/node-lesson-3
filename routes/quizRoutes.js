df60812d892cffce1c666b04e1021aba4af94e40
yoshin04 <taigaby0411@gmail.com>
2021-02-11 13:16:57 +0900

    test's

--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -5,3 +5,4 @@ const quizController = require('../controllers/quizController');
 router.get('/', quizController.index);
 router.get('/api', quizController.getQuizData);
 module.exports = router;
+//test
